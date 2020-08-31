2cfbcb288a24645ba37242672a3c173f42ffd202
kulloveth <kulloveth@gmail.com>
2020-08-31 11:55:54 +0100

    creating required fragments

---
+++ app/src/main/java/developer/kulloveth/com/gadsleaderboard/ui/fragments/LearningHoursLeaderBoardFragment.kt
@@ -0,0 +1,25 @@
+package developer.kulloveth.com.gadsleaderboard.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import developer.kulloveth.com.gadsleaderboard.R
+
+class LearningHoursLeaderBoardFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_learning_hours_leader_board, container, false)
+    }
+
+}
---
+++ app/src/main/java/developer/kulloveth/com/gadsleaderboard/ui/fragments/SplashFragment.kt
@@ -0,0 +1,26 @@
+package developer.kulloveth.com.gadsleaderboard.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import developer.kulloveth.com.gadsleaderboard.R
+
+class SplashFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    
+}

2a832506aa02db0a13a01125e253cf1f9659eba6
kulloveth <kulloveth@gmail.com>
2020-09-01 11:00:05 +0100

    create application koin, add base urls and integrate koin

---
+++ app/src/main/java/developer/kulloveth/com/gadsleaderboard/app/GadsLeaderBoardApp.kt
@@ -0,0 +1,16 @@
+package developer.kulloveth.com.gadsleaderboard.app
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class GadsLeaderBoardApp:Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+          androidContext(this@GadsLeaderBoardApp)
+            modules(listOf())
+        }
+    }
+}

625d564f40d734a280f2f4b068d2a38bdef2e3ed
kulloveth <kulloveth@gmail.com>
2020-09-08 08:49:39 +0100

    setup hours adapter

--- app/src/main/java/developer/kulloveth/com/gadsleaderboard/ui/adapter/LearningHoursAdapter.kt
+++ app/src/main/java/developer/kulloveth/com/gadsleaderboard/ui/adapter/LearningHoursAdapter.kt
@@ -1,16 +1,45 @@
 package developer.kulloveth.com.gadsleaderboard.ui.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.api.load
+import coil.transform.CircleCropTransformation
 import developer.kulloveth.com.gadsleaderboard.data.model.LearnerByHour
+import developer.kulloveth.com.gadsleaderboard.databinding.RvItemLayoutBinding
 
-class LearningHoursAdapter {
+class LearningHoursAdapter:ListAdapter<LearnerByHour,LearningHoursAdapter.LearningHourViewHolder>(
+    HOUR_DIFF) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LearningHourViewHolder {
+       val binding = RvItemLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return LearningHourViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: LearningHourViewHolder, position: Int) {
+      val hour = getItem(position)
+        holder.bindView(hour)
+    }
+
+
+
+    inner class LearningHourViewHolder(private val binding:RvItemLayoutBinding):RecyclerView.ViewHolder(binding.root){
+        fun bindView(hour: LearnerByHour){
+           binding.durationTv.text = hour.hours.toString()
+             binding.nameTv.text = hour.name.toString()
+             binding.ivLeader.load(hour.badgeUrl){
+                 transformations(CircleCropTransformation())
+             }
+         }
+    }
 
 
     companion object{
         private val HOUR_DIFF = object : DiffUtil.ItemCallback<LearnerByHour>(){
-            override fun areItemsTheSame(oldItem: LearnerByHour, newItem: LearnerByHour): Boolean {
+            override fun areItemsTheSame(oldItem: LearnerByHour, newItem: LearnerByHour): Boolean =
                 oldItem.hours == newItem.hours
-            }
 
             override fun areContentsTheSame(
                 oldItem: LearnerByHour,

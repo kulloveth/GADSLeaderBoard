71699947b0165ce7fa9dd25c451711ce00e7eea9
kulloveth <kulloveth@gmail.com>
2020-09-08 12:50:13 +0100

    create splash screen

--- app/src/main/java/developer/kulloveth/com/gadsleaderboard/ui/fragments/SplashFragment.kt
+++ app/src/main/java/developer/kulloveth/com/gadsleaderboard/ui/fragments/SplashFragment.kt
@@ -1,17 +1,21 @@
 package developer.kulloveth.com.gadsleaderboard.ui.fragments
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.navigation.findNavController
 import developer.kulloveth.com.gadsleaderboard.R
 
 class SplashFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    private val handler = Handler()
+    private val runnable = Runnable {
+        requireView().findNavController()
+            .navigate(R.id.action_splashFragment_to_leaderBoardFragment)
     }
 
     override fun onCreateView(
@@ -22,5 +26,20 @@ class SplashFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
-    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requireActivity().window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(runnable, 3000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+    }
+
+
 }

79fc598f743318749db6f1b3c9318bcda3ec18fa
kulloveth <kulloveth@gmail.com>
2020-09-15 21:47:14 +0100

    create result layout

--- app/src/main/java/developer/kulloveth/com/gadsleaderboard/ui/fragments/SubmissionFragment.kt
+++ app/src/main/java/developer/kulloveth/com/gadsleaderboard/ui/fragments/SubmissionFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
@@ -15,6 +16,7 @@ import developer.kulloveth.com.gadsleaderboard.databinding.FragmentSubmissionBin
 import developer.kulloveth.com.gadsleaderboard.ui.viewmodel.LearnersViewModel
 import developer.kulloveth.com.gadsleaderboard.util.Status
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
 
 class SubmissionFragment : Fragment() {
 
@@ -38,6 +40,34 @@ class SubmissionFragment : Fragment() {
     }
 
     private fun submission() {
+        val builder = AlertDialog.Builder(requireActivity())
+        val inflater = LayoutInflater.from(requireActivity())
+        val view = inflater.inflate(R.layout.confirm_submission_layout, null)
+        builder.setView(view)
+        builder.setCancelable(false)
+        val alertDialog = builder.create()
+        alertDialog.show()
+        val metrics = resources.displayMetrics
+        val width = metrics.widthPixels
+        val height = metrics.heightPixels
+        view.findViewById<ImageView>(R.id.cancel).setOnClickListener {
+            alertDialog.dismiss()
+        }
+        view.findViewById<Button>(R.id.btn).setOnClickListener {
+            if (binding?.emailEt?.text.isNullOrEmpty() || binding?.firstNameEt?.text.isNullOrEmpty() ||
+                binding?.lastNameEt?.text.isNullOrEmpty() || binding?.githubEt?.text.isNullOrEmpty()
+            ) {
+                Toast.makeText(requireContext(), "field must not be empty", Toast.LENGTH_LONG)
+                    .show()
+            } else {
+                viewModel.submitForm(
+                    binding?.emailEt?.text.toString(),
+                    binding?.firstNameEt?.text.toString(),
+                    binding?.lastNameEt?.text.toString(),
+                    binding?.githubEt?.text.toString()
+                )
+            }
+        }
         viewModel.fLivedata.observe(requireActivity(), Observer {
             when (it.status) {
 
@@ -49,28 +79,6 @@ class SubmissionFragment : Fragment() {
                 }
 
                 Status.LOADING -> {
-                    val builder = AlertDialog.Builder(requireActivity())
-                    val inflater = LayoutInflater.from(requireActivity())
-                    val view = inflater.inflate(R.layout.confirm_submission_layout, null)
-                    builder.setView(view)
-                    builder.setCancelable(false)
-                    val alertDialog = builder.create()
-                    alertDialog.show()
-                    view.findViewById<ImageView>(R.id.cancel).setOnClickListener {
-                    alertDialog.dismiss()
-                    }
-                    view.findViewById<Button>(R.id.yes_btn).setOnClickListener {
-                        if (binding?.emailEt?.text.isNullOrEmpty()|| binding?.firstNameEt?.text.isNullOrEmpty()||
-                            binding?.lastNameEt?.text.isNullOrEmpty()|| binding?.githubEt?.text.isNullOrEmpty())
-                            Toast.makeText(requireContext(),"field mus not be empty", Toast.LENGTH_LONG).show()
-                        else
-                        viewModel.submitForm(
-                            binding?.emailEt?.text.toString(),
-                            binding?.firstNameEt?.text.toString(),
-                            binding?.lastNameEt?.text.toString(),
-                            binding?.githubEt?.text.toString()
-                        )
-                    }
                 }
             }
         })
